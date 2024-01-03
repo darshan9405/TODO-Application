6641038db71c74a3dee2bc948bf8fb5b2a8d709d
darshan9405 <darshanb9405@gmail.com>
2024-01-03 17:06:47 +0530

    add database config and models

--- todo-app/src/main/java/my/learning/SimpleTodoConfiguration.java
+++ todo-app/src/main/java/my/learning/SimpleTodoConfiguration.java
@@ -1,10 +1,19 @@
 package my.learning;
 
 import io.dropwizard.core.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 
 public class SimpleTodoConfiguration extends Configuration {
     // TODO: implement service configuration
+    @NotNull
+    @Valid
+    private DataSourceFactory dataSourceFactory = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
+    }
 }
--- todo-app/src/main/java/my/learning/core/Task.java
+++ todo-app/src/main/java/my/learning/core/Task.java
@@ -1,5 +1,47 @@
 package my.learning.core;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
 public class Task {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JsonProperty("title")
+    @Column(name = "title")
+    private String title;
+
+    @JsonProperty("description")
+    @Column(name = "description")
+    private String description;
+
+    @JsonProperty("startDate")
+    @Column(name = "startDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startDate;
+
+    @JsonProperty("endDate")
+    @Column(name = "endDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endDate;
+
+    @JsonProperty("status")
+    @Column(name = "status")
+    private String status;
+
+    @JsonProperty("userId")
+    @ManyToOne
+    @JoinColumn(name = "userId", referencedColumnName = "userName")
+    private User userId;
 }
